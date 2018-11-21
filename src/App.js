eee968d20d8010cccc8b7510a98ea66dd708aa84
Saib Ahmed <saibahmed1512@gmail.com>
2018-11-21 13:00:33 +0530

    added counters props in App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,49 @@ import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handelIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>
     );
   }
+  handelIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counter });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
 }
 
 export default App;
