4fc350be9a968efea09a70e8771c735454589478
Saib Ahmed <saibahmed1512@gmail.com>
2018-11-21 13:01:30 +0530

    added totalCounter number on Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,12 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1">Navbar</span>
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
       </nav>
     );
   }
